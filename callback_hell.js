e4473d0a81f251dd825755f7c846a6604f6d1d2c
주정준 <65483521+jun1485@users.noreply.github.com>
2023-05-30 08:48:12 +0900

    Update callback_hell.js

--- callback_hell.js
+++ callback_hell.js
@@ -1,6 +1,9 @@
 // 콜백 지옥이란, 여러 개의 비동기 작업을 순차적으로 처리하거나, 서로 의존성을 가지는 경우에
 // 콜백 함수를 계속 중첩해야 하는 상황. (복잡성 +, 가독성 -)
 
+
+// 세 개의 파일을 순차적으로 읽음
+// 각 파일을 읽는 작업은 이전 파일의 작업이 완료되어야 시작.
 const fs = require('fs');
 
 fs.readFile('file1.txt', 'utf8', function(err, data) {
@@ -25,3 +28,10 @@ fs.readFile('file1.txt', 'utf8', function(err, data) {
     });
   }
 });
+
+
+// 아래는 콜백 지옥의 개선책
+// fs.readFile 파일을 Promise로 감싸는 함수.
+const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
