2bddc6a9aca75b54bb04a969a7ae8066e73e5358
주정준 <65483521+jun1485@users.noreply.github.com>
2023-06-01 08:57:35 +0900

    Create closure.js

---
+++ closure.js
@@ -0,0 +1,9 @@
+// JS에서 함수와 함수가 선언된 어휘적 환경의 조합.
+// 이를 통해 JS에서 함수는 바깥 스코프에서 선언된 변수에 접근 가능.
+
+function outerFunction(outerVariable) {
+  return function innerFunction(innerVariable) {
+    console.log('outerVariable:', outerVariable);
+    console.log('innerVariable:', innerVariable);
+  }
+}
