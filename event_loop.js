f628ebf97745d10fb3c5d3e49074407398fc9f55
주정준 <65483521+jun1485@users.noreply.github.com>
2023-06-12 01:20:07 +0900

    Update event_loop.js

--- event_loop.js
+++ event_loop.js
@@ -3,3 +3,20 @@
 // setTimeout은 콜백 함수를 매크로 테스크 큐에 추가.
 // 그 콜백이 시작되려면 현재 실행중인 코드가 종료된 후 이벤트 루프가 매크로 테스크 큐를 처리할 때까지 대기.
 
+console.log('1: Start script');
+
+setTimeout(() => {
+  console.log('2: In the timeout');
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log('3: In the Promise');
+});
+
+console.log('4: End script');
+
+// 아래는 콘솔 결과
+// 1: Start script
+// 4: End script
+// 3: In the Promise
+// 2: In the timeout
